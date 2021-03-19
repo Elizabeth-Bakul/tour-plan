7258e30bb3e421c514508f8b3438b169e0561819
Elizabeth-Bakul <bakelal@mail.ru>
2021-03-19 15:57:50 +0300

    create: modal form-booking

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  spaceBetween: 10,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
-var reviewSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  spaceBetween: 10,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 10,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+    },
+  });
+  var reviewSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 10,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("CLICK");
-  var menu = document.querySelector(".navbar-bottom");
-  menu.classList.toggle("navbar-bottom--visible");
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("CLICK");
+    var menu = document.querySelector(".navbar-bottom");
+    menu.classList.toggle("navbar-bottom--visible");
+  });
+
+  var modalButton = $('[data-toggle="modal"]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on("click", function(event){
+    event.preventDefault();
+    closeModal();
+  });
+  $(document).keyup(function (e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+      closeModal();
+    }
+  });
+  function openModal(){
+    var targetModal=$(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 
 });
